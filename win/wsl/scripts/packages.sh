f697cf8103173f78d30f7f20af79cd7bb54b0e94
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-08-04 19:59:03 +1000

    feat: add antigen for zsh

--- win/wsl/scripts/packages.sh
+++ win/wsl/scripts/packages.sh
@@ -17,3 +17,8 @@ sudo apt install -y \
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+# zsh
+sudo chsh -s $(which zsh)
+curl -L git.io/antigen > ${HOME}/antigen.zsh
+
+
