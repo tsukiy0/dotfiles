07a34c90235aa4f5045a8f356f64caee84b9b26b
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-08-12 01:14:32 +1000

    feat: install gnome-tweak-tool to make capslock an esc

--- ubuntu/scripts/setup.sh
+++ ubuntu/scripts/setup.sh
@@ -14,7 +14,8 @@ sudo apt install -y \
     jq \
     curl \
     docker.io \
-    chromium-browser
+    chromium-browser \
+    gnome-tweak-tool
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
