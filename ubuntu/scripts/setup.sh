7cda481488c166894fd08f9ae2dfa799e554b460
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-08-13 19:33:59 +1000

    feat: install dotnet

--- ubuntu/scripts/setup.sh
+++ ubuntu/scripts/setup.sh
@@ -13,13 +13,41 @@ sudo apt install -y \
     zip \
     jq \
     curl \
-    docker.io \
     chromium-browser \
     gnome-tweak-tool \
     caffeine \
     vlc \
-    docker-compose \
     build-essential
+
+function install_dotnet() {
+    if ! command -v dotnet &> /dev/null
+    then
+        PKG_MS_PROD_PATH=/tmp/packages-microsoft-prod.deb
+
+        wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O ${PKG_MS_PROD_PATH}
+        sudo dpkg -i ${PKG_MS_PROD_PATH}
+        rm -rf ${PKG_MS_PROD_PATH}
+
+        sudo apt update
+        sudo apt install -y \
+            dotnet-sdk-3.1
+    fi
+}
+
+function install_docker() {
+    if ! command -v docker &> /dev/null
+    then
+        sudo apt update
+        sudo apt install -y \
+            docker.io \
+            docker-compose
+
+        sudo usermod -aG docker $USER
+    fi
+}
+
+install_dotnet
+install_docker
 
 sudo snap install --classic code
 sudo snap install discord
@@ -33,4 +61,3 @@ sudo chsh -s $(which zsh)
 curl -L git.io/antigen > ${HOME}/antigen.zsh
 
 # docker
-sudo usermod -aG docker $USER
