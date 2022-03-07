ffe729dc8003957976002074ba5992e55bac8061
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-03-07 06:53:52 -0800

    feat: add win os

---
+++ win/scripts/apps1.ps1
@@ -0,0 +1,16 @@
+choco install -y git
+choco install -y awscli
+choco install -y ffmpeg
+choco install -y vim
+
+choco install -y microsoft-windows-terminal
+choco install -y googlechrome
+choco install -y vscode
+choco install -y powertoys
+choco install -y docker-desktop
+choco install -y vlc
+choco install -y steam
+choco install -y dbeaver
+choco install -y slack
+choco install -y postman
+choco install -y via
---
+++ win/scripts/config.ps1
@@ -0,0 +1,6 @@
+# home
+Copy-Item -Path $PSScriptRoot/../home/** -Destination $HOME -Include **
+
+# keyboard repeat
+reg add "HKCU\Control Panel\Keyboard" /v KeyboardDelay /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Keyboard" /v KeyboardSpeed /t REG_SZ /d "31" /f
